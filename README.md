9a99d0f4bf6938d08c1d138d07e3c2807cbd59ce
lmphatsi <la.mphatsi@gmail.com>
2023-12-21 13:43:42 +0200

    init openmrs basic module

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-openmrs-module-basicmodule
+openmrs-module-mphatsi
 ==========================
 
 A demonstration module for new OpenMRS module developers
--- api/src/main/java/org/openmrs/module/basicmodule/BasicModuleActivator.java
+++ api/src/main/java/org/openmrs/module/basicmodule/BasicModuleActivator.java
@@ -11,7 +11,7 @@
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
-package org.openmrs.module.basicmodule;
+package org.openmrs.module.mphatsi;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
